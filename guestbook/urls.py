442bd4eb35db33a6c65a48db1642571dd1cf9b11
Dino Wei <11130270@dcsh.tp.edu.tw>
2024-07-04 11:17:33 +0800

    定義 Message  資料模型

--- guestbook/urls.py
+++ guestbook/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('message/', unclude('message.urls'))
+    path('message/', include('message.urls'))
 ]
