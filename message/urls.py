9063299dc1323ec40b8b3be1fe153c253c6328c7
Dino Wei <11130270@dcsh.tp.edu.tw>
2024-07-04 13:14:01 +0800

    留言列表初版

--- message/urls.py
+++ message/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    # massage/ => 留言列表
+    path('', views.MessageList.as_view(), name='msg_list'),
 ]
--- message/views.py
+++ message/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Message 
 
 # Create your views here.
+class MessageList(ListView):
+    model = Message
