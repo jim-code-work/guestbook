7a3fe468fccdc3c5920b2ac2cb06c7117c0c4776
Dino Wei <11130270@dcsh.tp.edu.tw>
2024-07-04 14:02:02 +0800

    123

--- message/views.py
+++ message/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Message 
 
 # Create your views here.
 class MessageList(ListView):
     model = Message
+
+
+class MessageRead(DetailView):
+    model = Message
+
