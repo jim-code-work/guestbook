b6e5ea1a07c9cac38718df723a77fb42a3178fae
Dino Wei <11130270@dcsh.tp.edu.tw>
2024-07-05 11:07:52 +0800

    123456

--- guestbook/urls.py
+++ guestbook/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('message/', include('message.urls'))
+    path('message/', include('message.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    
 ]
