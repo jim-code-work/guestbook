ce3dba00deac1b2d8470eb9a1c5d2eca3a6ffaa0
Dino Wei <11130270@dcsh.tp.edu.tw>
2024-07-05 11:02:39 +0800

    12345

--- message/urls.py
+++ message/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     # massage/ => 留言列表
     path('', views.MessageList.as_view(), name='msg_list'),
     path('<int:pk>/', views.MessageRead.as_view(), name="msg_read"),
+    path('create', views.MessageNew.as_view(), name='msg_create'),
+     path('<int:pk>/delete', views.MessageDelete.as_view(), name="msg_delete"),
 ]
--- message/views.py
+++ message/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Message 
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from .models import Message
+from django.urls import reverse_lazy
 
 # Create your views here.
 class MessageList(ListView):
@@ -9,4 +10,15 @@ class MessageList(ListView):
 
 class MessageRead(DetailView):
     model = Message
+    field = ['user', 'receipt', 'subject', 'content']
+    success_urls = "/message/"
+
+class MessageNew(CreateView):
+    model = Message
+    fields = ['user', 'receipt', 'subject', 'content']
+    success_url = reverse_lazy('msg_list')
+
+class MessageDelete(DeleteView):
+    model = Message
+    success_urls = "/message/"
 
