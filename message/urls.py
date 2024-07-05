a57dff15c610fce4847b6b1f92d22d5de1d5f9d9
Dino Wei <11130270@dcsh.tp.edu.tw>
2024-07-05 11:59:55 +0800

    123456

--- message/urls.py
+++ message/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # massage/ => 留言列表
+    # message/  =>留言列表
     path('', views.MessageList.as_view(), name='msg_list'),
+    # message/1/, message/2/, ...
     path('<int:pk>/', views.MessageRead.as_view(), name="msg_read"),
-    path('create', views.MessageNew.as_view(), name='msg_create'),
-     path('<int:pk>/delete', views.MessageDelete.as_view(), name="msg_delete"),
+    # message/create/
+    path('create',views.MessageNew.as_view(), name='msg_create'),
+    #message/1/delete/, message/2/delete/, ...
+    path('<int:pk>/delete/', views.MessageDelete.as_view(), name='msg_delete')
 ]
--- message/views.py
+++ message/views.py
@@ -1,24 +1,36 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from .models import Message
 from django.urls import reverse_lazy
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 class MessageList(ListView):
     model = Message
 
+    ordering = ['created', 'id']
+    #message/mesage_list.html
+    #頁面範本檔案名稱應用程式/資料模型_.html => message/message_list.html
+    #頁面範本變數名稱資料模型_list => message_list
 
 class MessageRead(DetailView):
     model = Message
-    field = ['user', 'receipt', 'subject', 'content']
-    success_urls = "/message/"
 
+        #頁面範本檔案名稱應用程式/資料模型_detail.html => message/message_detail.html
+        #頁面範本變數名稱資料模型_list => message
 class MessageNew(CreateView):
     model = Message
     fields = ['user', 'receipt', 'subject', 'content']
-    success_url = reverse_lazy('msg_list')
+    
+    success_url = reverse_lazy('msg_list') 
+    # field = '__all__'
+    #頁面範本檔案名稱:應用程式/資料模型_form.html => message/message_detail.html
+    #頁面範本變數名稱:form
 
-class MessageDelete(DeleteView):
+
+class MessageDelete(LoginRequiredMixin, DeleteView):
     model = Message
-    success_urls = "/message/"
+    success_url = '/message/'
+
+        #頁面範本檔案名稱:應用程式/資料模型_confirm_delete.html => message/message_confirm_delete.html
+        #頁面範本變數名稱:form, object, 資料模型(小寫)
 
