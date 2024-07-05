d8a1a93069f335b7a002a4cc2c897e03fa08afdb
Dino Wei <11130270@dcsh.tp.edu.tw>
2024-07-05 13:24:21 +0800

    final

--- guestbook/urls.py
+++ guestbook/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('message/', include('message.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    
+    path('', RedirectView.as_view(url='message/'))
 ]
